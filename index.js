1aceea2ce67677225b5db2b2cf04e8774161e51e
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-09-29 17:23:51 +0200

    Fallback image and author inside catch(), in case the unsplash API fails

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ try {
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
 } catch (err) {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
+    // Fallback image and author in case the unsplash API fails
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
     document.getElementById("author").textContent = `By: Neo Radebe`
 }
 
