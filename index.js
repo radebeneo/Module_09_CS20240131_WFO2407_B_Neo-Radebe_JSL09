85722c25572b992c7eadc8e27ed8c79579484254
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-09-29 17:21:43 +0200

    Set the background image and display the author  name

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 try {
     const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     const data = await res.json()
+
+    // Set the background image and display the author's name
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
 } catch (err) {
